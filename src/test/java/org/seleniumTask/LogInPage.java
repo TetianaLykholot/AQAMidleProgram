0d11871e4612745f300f2ddd5aa7e41ca2b56ae1
Tetiana_Lykholot <liholet.ta@gmail.com>
2025-06-29 18:52:21 +0200

    Test2

--- src/test/java/org/seleniumTask/LogInPage.java
+++ src/test/java/org/seleniumTask/LogInPage.java
@@ -8,34 +8,34 @@ import org.openqa.selenium.support.locators.RelativeLocator;
 
 public class LogInPage  {
 
-   private WebDriver driver;
-
-   public LogInPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    private By emailField = By.id("CustomerEmail");
-    private By loginButton = By.className("login-page__sign-in-button");
-
-    public void enterEmail() {
-        driver.findElement(emailField).sendKeys("testli@yopmail.com");
-    }
-
-    @Step("Step 1: Navigate to Login Page")
-    public void enterPassword() {
-        WebElement passwordField = driver.findElement(
-                RelativeLocator.with(By.tagName("input")).below(emailField));
-        passwordField.sendKeys("Test123!");
-    }
-
-    public void clickLogin() {
-        driver.findElement(loginButton).click();
-    }
-
-    @Step("Login to the page")
-    public void login(){
-        enterEmail();
-        enterPassword();
-        clickLogin();
-    }
+//   private WebDriver driver;
+//
+//   public LogInPage(WebDriver driver) {
+//        this.driver = driver;
+//    }
+//
+//    private By emailField = By.id("CustomerEmail");
+//    private By loginButton = By.className("login-page__sign-in-button");
+//
+//    public void enterEmail() {
+//        driver.findElement(emailField).sendKeys("testli@yopmail.com");
+//    }
+//
+//    @Step("Step 1: Navigate to Login Page")
+//    public void enterPassword() {
+//        WebElement passwordField = driver.findElement(
+//                RelativeLocator.with(By.tagName("input")).below(emailField));
+//        passwordField.sendKeys("Test123!");
+//    }
+//
+//    public void clickLogin() {
+//        driver.findElement(loginButton).click();
+//    }
+//
+//    @Step("Login to the page")
+//    public void login(){
+//        enterEmail();
+//        enterPassword();
+//        clickLogin();
+//    }
 }
--- src/test/java/org/seleniumTask/NewEraLoginTest.java
+++ src/test/java/org/seleniumTask/NewEraLoginTest.java
@@ -6,37 +6,45 @@ import org.testng.annotations.Test;
 
 
 
-@Listeners({com.epam.reportportal.testng.ReportPortalTestNGListener.class, CustomTestListener.class})
-public class NewEraLoginTest extends NewEraBaseTest {
 
-    private LogInPage loginPage;
-    private Products products;
+//@Listeners({com.epam.reportportal.testng.ReportPortalTestNGListener.class, CustomTestListener.class})
+public class NewEraLoginTest  {
 
-    @BeforeMethod
-    public void initializePageObjects() {
-        if (driver == null) {
-            throw new IllegalStateException("WebDriver is not initialized. Check test setup.");
-        }
-        loginPage = new LogInPage(driver);
-        products = new Products(driver);
+    @Test
+    public void test() {
+        System.out.println("This is a test method.");
     }
 
 
-    @Epic("Regression Test")
-    @Feature("Login Test")
-    @Severity(SeverityLevel.BLOCKER)
-    @Description ("Test Description: Login test with correct user name and password ")
-    @Step("Step 1: Navigate to Login Page")
-    @Test(priority = 1, description = "Valid login to New Era", testName = "Login")
-    public void testValidLogin()  {
-        loginPage.login();
-    }
-
-    @Description ("Test Description: Adding product to the cart ")
-    @Feature("Cart Page")
-    @Step("Step: add products to the bag ")
-    @Test(testName = "Adding to basket test")
-    public void addProductToBasket()  {
-        products.addToBag();
-    }
+//
+//    private LogInPage loginPage;
+//    private Products products;
+//
+//    @BeforeMethod
+//    public void initializePageObjects() {
+//        if (driver == null) {
+//            throw new IllegalStateException("WebDriver is not initialized. Check test setup.");
+//        }
+//        loginPage = new LogInPage(driver);
+//        products = new Products(driver);
+//    }
+//
+//
+//    @Epic("Regression Test")
+//    @Feature("Login Test")
+//    @Severity(SeverityLevel.BLOCKER)
+//    @Description ("Test Description: Login test with correct user name and password ")
+//    @Step("Step 1: Navigate to Login Page")
+//    @Test(priority = 1, description = "Valid login to New Era", testName = "Login")
+//    public void testValidLogin()  {
+//        loginPage.login();
+//    }
+//
+//    @Description ("Test Description: Adding product to the cart ")
+//    @Feature("Cart Page")
+//    @Step("Step: add products to the bag ")
+//    @Test(testName = "Adding to basket test")
+//    public void addProductToBasket()  {
+//        products.addToBag();
+//    }
 }
